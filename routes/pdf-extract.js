0d5d2e05e1cef3d68a1c86f709fa32ce7a34be65
iqbal-rashed <dev.rashediqbal@gmail.com>
2022-12-08 14:46:29 +0600

    add bin

--- routes/pdf-extract.js
+++ routes/pdf-extract.js
@@ -3,7 +3,6 @@ const multer = require("multer");
 const { StatusError } = require("../utils");
 const ExtractPdf = require("../utils/ExtractPdf");
 const TextToJson = require("../utils/TextToJson");
-const os = require("os");
 
 const upload = multer({ storage: multer.memoryStorage() });
 
@@ -12,26 +11,20 @@ router.post("/pdf-extract", upload.single("pdf"), async (req, res) => {
         if (!req.file) throw StatusError("File not found");
         const pdfBuffer = req.file.buffer;
 
-        // const extractPdf = new ExtractPdf(pdfBuffer);
-        // const images = await extractPdf.extractImages();
-        // const text = await extractPdf.extractText();
+        const extractPdf = new ExtractPdf(pdfBuffer);
+        const images = await extractPdf.extractImages();
+        const text = await extractPdf.extractText();
 
-        // const textToJson = new TextToJson(text);
+        const textToJson = new TextToJson(text);
 
-        // const details = textToJson.getDetails();
-        const arch = os.arch();
-        const platform = os.platform();
-        const processPlatform = process.platform;
+        const details = textToJson.getDetails();
 
         res.status(200).send({
-            // ...details,
+            ...details,
             // images: {
             //     profile: images[0],
             //     signature: images[1],
             // },
-            arch,
-            platform,
-            processPlatform,
         });
     } catch (err) {
         res.status(err.status || 500).send(
